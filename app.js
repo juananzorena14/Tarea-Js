03942d1299250c4021946f378044f1ce86340d78
Juan Anzorena <chinooanzorena@gmail.com>
2023-06-27 17:11:53 -0300

    primer commit

---
+++ app.js
@@ -0,0 +1,17 @@
+let num1= 219
+let num2= 574
+//SUMA
+console.log (num1+num2)
+//RESTA
+console.log (num1-num2)
+//DIVISION
+console.log (num1/num2)
+//MULTIPLICACION
+console.log (num1*num2)
+//MAYOR O IGUAL
+console.log (num1>=num2)
+//MENOR O IGUAL
+console.log (num1<=num2)
+//DISTINTO
+console.log (num1!=num2)
+
---
+++ tarea14.js
@@ -0,0 +1,21 @@
+//TAREA JAVASCRYPT
+
+// Tarea 1
+// ------------
+// 1 - ingreso el apellido
+let apellido = "Anzorena";
+console.log(apellido);
+// 2 - calcular cantidad de caracteres
+console.log(apellido.length);
+// 3 - obtener la última letra del apellido
+console.log(apellido.at(-1));
+
+
+// Tarea 2
+// ------------
+// Dada la siguiente palabra "marmota"
+let palabra = "marmota";
+// 1 - Pasarla a mayuscula
+console.log(palabra.toUpperCase());
+// 2 - Capitalizar la palabra (Marmota)
+console.log(palabra.at(0).toUpperCase() + palabra.substring(1));
